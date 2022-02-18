78046c2a003e03b5531ab2168566c4755611da92
Carlos Dubón <holacarlosdubon@gmail.com>
2022-02-17 18:37:37 -0600

    :large_blue_circle: landing circles

---
+++ src/components/atoms/Header.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { css } from "@emotion/react";
+
+const Header: FC = () => {
+  return (
+    <div css={css``}>
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export { Header };
