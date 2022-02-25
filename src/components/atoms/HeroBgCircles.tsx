cc192363fe2f34144968083706a6de106847bacc
Carlos Dubón <holacarlosdubon@gmail.com>
2022-02-24 18:11:41 -0600

    :hammer: fix hero circles

--- src/components/atoms/HeroBgCircles.tsx
+++ src/components/atoms/HeroBgCircles.tsx
@@ -10,17 +10,23 @@ const HeroBgCircles: FC = () => {
       css={css`
         top: 0;
         position: absolute;
+        display: flex;
+        justify-content: flex-end;
         z-index: -1;
+        overflow: hidden;
+        @media (min-width: ${Screens.sm}px) {
+          overflow: visible;
+        }
       `}
     >
       <div
         css={css`
-          position: absolute;
-          top: 0;
-          right: 0;
-          transform: translateX(150px) translateY(-150px);
+          transform: translateX(140px) translateY(-120px);
           @media (min-width: ${Screens.sm}px) {
-            transform: translateX(295px) translateY(-295px);
+            transform: translateX(195px) translateY(-195px);
+          }
+          @media (min-width: ${Screens.md}px) {
+            transform: translateX(350px) translateY(-295px);
           }
         `}
       >
