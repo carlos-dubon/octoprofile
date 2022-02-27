cdd9e07385f1e21db6231e945b774fd1b2fa11ca
Carlos Dubón <holacarlosdubon@gmail.com>
2022-02-27 14:27:48 -0600

    fix footer styles on mobile

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { css } from "@emotion/react";
 import Colors from "@app/styles/colors";
+import Screens from "@app/styles/breakpoints";
 import Link from "next/link";
 import Image from "next/image";
 import logoWhite from "public/logoWhite.svg";
@@ -16,43 +17,84 @@ const Footer: FC = () => {
   `;
 
   const footerStyles = css`
-    display: flex;
+    display: grid;
+    justify-content: center;
+    padding-top: 2em;
+    padding-bottom: 2em;
+    gap: 1.2em;
+    grid-template-areas:
+      "copyright"
+      "logo"
+      "gitHub";
     color: #ffffff;
-    justify-content: space-between;
+    @media (min-width: ${Screens.lg}px) {
+      grid-template-areas: "logo copyright gitHub";
+      justify-content: space-between;
+    }
+  `;
+
+  const logoStyles = css`
+    grid-area: logo;
+    width: 100;
+    display: flex;
+    justify-content: center;
     align-items: center;
-    min-height: 72px;
+  `;
+
+  const copyrightStyles = css`
+    margin: 0;
+    grid-area: copyright;
+    text-align: center;
+    width: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const gitHubStyles = css`
+    grid-area: gitHub;
+    width: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   `;
 
   return (
     <div css={containerStyles}>
       <div className="container" css={footerStyles}>
-        <Link href="/">
-          <a>
-            <Image
-              src={logoWhite}
-              alt={"Logo white"}
-              width={136.41}
-              height={28}
-            />
-          </a>
-        </Link>
-        <p>© {new Date().getFullYear()} OctoProfile. All rights reserved</p>
-        <TranslucentNavLink>
-          <div
-            css={css`
-              display: grid;
-              align-items: center;
-              width: 2em;
-            `}
-          >
-            <Image
-              src="/icons/github.svg"
-              alt="github"
-              width={18}
-              height={18}
-            />
-          </div>
-        </TranslucentNavLink>
+        <div css={logoStyles}>
+          <Link href="/">
+            <a>
+              <Image
+                src={logoWhite}
+                alt={"Logo white"}
+                width={136.41}
+                height={28}
+              />
+            </a>
+          </Link>
+        </div>
+        <p css={copyrightStyles}>
+          © {new Date().getFullYear()} OctoProfile. All rights reserved
+        </p>
+        <div css={gitHubStyles}>
+          <TranslucentNavLink>
+            <div
+              css={css`
+                display: grid;
+                align-items: center;
+                width: 2em;
+              `}
+            >
+              <Image
+                src="/icons/github.svg"
+                alt="github"
+                width={18}
+                height={18}
+              />
+            </div>
+          </TranslucentNavLink>
+        </div>
       </div>
     </div>
   );
