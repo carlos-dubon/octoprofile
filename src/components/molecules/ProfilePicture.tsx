9aa121b4dd0617c4c932055d7ccec298fb1e1373
Carlos Dubón <holacarlosdubon@gmail.com>
2022-03-27 17:43:07 -0600

    profile picture component 😃

---
+++ src/components/molecules/ProfilePicture.tsx
@@ -0,0 +1,60 @@
+import { FC } from "react";
+import Colors from "@app/styles/colors";
+import { css } from "@emotion/react";
+import Skeleton from "react-loading-skeleton";
+import Image from "next/image";
+import { useAppSelector } from "@app/hooks";
+
+const ProfilePicture: FC = () => {
+  const containerStyles = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ffffff;
+    width: 225px;
+    height: 225px;
+    border-radius: 50%;
+    margin-top: 3.75em;
+    box-shadow: 0px 10px 20px rgba(41, 41, 42, 0.07);
+  `;
+
+  const user = useAppSelector((state) => state.user);
+  const loadingUser = useAppSelector((state) => state.loaders.loadingUser);
+
+  return (
+    <div css={containerStyles}>
+      {loadingUser ? (
+        <div
+          css={css`
+            width: 208px;
+            height: 208px;
+            background: ${Colors.grayHover};
+            border-radius: 50%;
+          `}
+        >
+          <Skeleton
+            width={208}
+            height={208}
+            circle
+            css={css`
+              position: relative;
+              top: -2px;
+            `}
+          />
+        </div>
+      ) : (
+        <Image
+          src={user.avatarUrl}
+          alt={user.displayName}
+          width={210}
+          height={210}
+          css={css`
+            border-radius: 50%;
+          `}
+        />
+      )}
+    </div>
+  );
+};
+
+export { ProfilePicture };
--- src/components/templates/UserHero.tsx
+++ src/components/templates/UserHero.tsx
@@ -3,8 +3,16 @@ import { css } from "@emotion/react";
 import Colors from "@app/styles/colors";
 import Image from "next/image";
 import { TranslucentNavLink } from "@lib/atoms";
+import { useAppSelector } from "@app/hooks";
+import Skeleton from "react-loading-skeleton";
+import Link from "next/link";
+import Tippy from "@tippyjs/react";
+import { ProfilePicture } from "@lib/molecules";
 
 const UserHero: FC = () => {
+  const user = useAppSelector((state) => state.user);
+  const loadingUser = useAppSelector((state) => state.loaders.loadingUser);
+
   const containerStyles = css`
     display: flex;
     flex-direction: column;
@@ -22,6 +30,29 @@ const UserHero: FC = () => {
     background: ${Colors.purple900};
   `;
 
+  const contentWrapperStyles = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  `;
+
+  const navContainerStyles = css`
+    display: flex;
+    width: 100%;
+    justify-content: flex-end;
+    margin-top: 1em;
+  `;
+
+  const navButtonContainerStyles = css`
+    display: flex;
+    gap: 0.5em;
+    padding: 0 0.75em;
+    height: 2em;
+    align-items: center;
+    font-size: 0.875em;
+  `;
+
   return (
     <div css={containerStyles}>
       <div css={wavesStyles}>
@@ -34,35 +65,85 @@ const UserHero: FC = () => {
         />
       </div>
       <div className="container">
-        <div
-          css={css`
-            display: flex;
-            width: 100%;
-            justify-content: flex-end;
-            margin-top: 1em;
-          `}
-        >
-          <TranslucentNavLink href="https://github.com/carlos-dubon/octoprofile">
+        <div css={contentWrapperStyles}>
+          <div css={navContainerStyles}>
+            <TranslucentNavLink href="https://github.com/carlos-dubon/octoprofile">
+              <div css={navButtonContainerStyles}>
+                <Image
+                  src="/icons/github.svg"
+                  alt="github"
+                  width={18}
+                  height={18}
+                  priority
+                />
+                <div className="hide-mobile">Contribute</div>
+              </div>
+            </TranslucentNavLink>
+          </div>
+
+          <ProfilePicture />
+
+          {loadingUser ? (
             <div
               css={css`
-                display: flex;
-                gap: 0.5em;
-                padding: 0 0.75em;
-                height: 2em;
-                align-items: center;
-                font-size: 0.875em;
+                width: 330px;
+                height: 59px;
+                margin-top: 0.5em;
+                margin-bottom: 0.2em;
+                font-size: 3em;
               `}
             >
-              <Image
-                src="/icons/github.svg"
-                alt="github"
-                width={18}
-                height={18}
-                priority
-              />
-              <div className="hide-mobile">Contribute</div>
+              <Skeleton width={330} height={59} />
             </div>
-          </TranslucentNavLink>
+          ) : (
+            <h1
+              css={css`
+                font-size: 3em;
+                font-weight: 800;
+                width: fit-content;
+                margin-top: 0.5em;
+                margin-bottom: 0.2em;
+                text-align: center;
+              `}
+            >
+              {user.displayName}
+            </h1>
+          )}
+
+          {loadingUser ? (
+            <div
+              css={css`
+                width: 140px;
+                height: 29px;
+                font-size: 1.5em;
+                font-size: 3em;
+              `}
+            >
+              <Skeleton width={140} height={29} />
+            </div>
+          ) : (
+            <Link href={`https://github.com/${user.username}`} passHref>
+              <a
+                target="_blank"
+                css={css`
+                  font-size: 1.5em;
+                  font-weight: 500;
+                  width: fit-content;
+                  :hover {
+                    text-decoration: underline;
+                  }
+                `}
+              >
+                <Tippy
+                  content="View GitHub profile"
+                  delay={200}
+                  offset={[0, 12]}
+                >
+                  <span>@{user.username}</span>
+                </Tippy>
+              </a>
+            </Link>
+          )}
         </div>
       </div>
     </div>
