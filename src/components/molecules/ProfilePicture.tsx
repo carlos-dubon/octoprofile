ee35a07df93f0cbcf44d21634c6e2372d4bb3d5a
Carlos Dubón <holacarlosdubon@gmail.com>
2022-04-06 12:42:53 -0600

    responsive profile picture

--- src/components/molecules/ProfilePicture.tsx
+++ src/components/molecules/ProfilePicture.tsx
@@ -3,6 +3,7 @@ import { css } from "@emotion/react";
 import Skeleton from "react-loading-skeleton";
 import Image from "next/image";
 import { useGetUser } from "@app/hooks";
+import Screens from "@app/styles/breakpoints";
 
 const ProfilePicture: FC = () => {
   const containerStyles = css`
@@ -10,11 +11,43 @@ const ProfilePicture: FC = () => {
     justify-content: center;
     align-items: center;
     background-color: #ffffff;
-    width: 225px;
-    height: 225px;
+    width: 210px;
+    height: 210px;
+    @media (min-width: ${Screens.sm}px) {
+      width: 225px;
+      height: 225px;
+    }
+    position: relative;
     border-radius: 50%;
     margin-top: 4em;
     box-shadow: 0px 10px 20px rgba(41, 41, 42, 0.07);
+  `;
+
+  const skeletonContainerStyles = css`
+    width: 194px;
+    height: 194px;
+    border-radius: 50%;
+    @media (min-width: ${Screens.sm}px) {
+      width: 208px;
+      height: 208px;
+    }
+  `;
+
+  const skeletonStyles = css`
+    position: relative;
+    top: -2px;
+  `;
+
+  const imageContainerStyles = css`
+    width: 194px;
+    height: 194px;
+    @media (min-width: ${Screens.sm}px) {
+      width: 210px;
+      height: 210px;
+    }
+    position: relative;
+    border-radius: 50%;
+    overflow: hidden;
   `;
 
   const [user, loading] = useGetUser();
@@ -22,33 +55,13 @@ const ProfilePicture: FC = () => {
   return (
     <div css={containerStyles}>
       {loading ? (
-        <div
-          css={css`
-            width: 208px;
-            height: 208px;
-            border-radius: 50%;
-          `}
-        >
-          <Skeleton
-            width={208}
-            height={208}
-            circle
-            css={css`
-              position: relative;
-              top: -2px;
-            `}
-          />
+        <div css={skeletonContainerStyles}>
+          <Skeleton width="100%" height="100%" circle css={skeletonStyles} />
         </div>
       ) : (
-        <Image
-          src={user.avatarUrl}
-          alt={user.displayName}
-          width={210}
-          height={210}
-          css={css`
-            border-radius: 50%;
-          `}
-        />
+        <div css={imageContainerStyles}>
+          <Image src={user.avatarUrl} alt={user.displayName} layout="fill" />
+        </div>
       )}
     </div>
   );
