56ec7b46bdf041f6d6d603192b21aee2ce5c9891
Carlos Dubón <holacarlosdubon@gmail.com>
2022-04-07 16:23:12 -0600

    most used languages chart

--- src/helpers/getMostUsedLanguages.ts
+++ src/helpers/getMostUsedLanguages.ts
@@ -1,6 +1,9 @@
+import { AppChartData } from "@app/types/chartjs";
 import { Repo } from "src/state/slices/reposSlice";
+import ghLangColors from "gh-lang-colors";
+import { colord } from "colord";
 
-const getMostUsedLanguages = (repos: Repo[]): [string, number][] => {
+const getMostUsedLanguages = (repos: Repo[]): AppChartData => {
   const languages = repos.reduce((acc, repo) => {
     if (repo.language && !repo.isFork) {
       acc[repo.language] = acc[repo.language] + 1 || 1;
@@ -12,7 +15,32 @@ const getMostUsedLanguages = (repos: Repo[]): [string, number][] => {
     .sort((a, b) => b[1] - a[1])
     .slice(0, 10);
 
-  return sortedLanguages;
+  const chartData: AppChartData = sortedLanguages.reduce(
+    (prev: AppChartData, [language, count]) => {
+      const langColor: string = colord(
+        ghLangColors[language as keyof typeof ghLangColors] || "rgba(0,0,0,0.7)"
+      )
+        .alpha(0.7)
+        .toRgbString();
+
+      prev.labels.push(language);
+      prev.datasets[0].data.push(count);
+      prev.datasets[0].backgroundColor.push(langColor);
+
+      return prev;
+    },
+    {
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          backgroundColor: [],
+        },
+      ],
+    }
+  );
+
+  return chartData;
 };
 
 export { getMostUsedLanguages };
