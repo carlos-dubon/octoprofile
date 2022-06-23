37fa80cec8ec7761c0b80e5302fb102219c4fcc3
Carlos Dubón <holacarlosdubon@gmail.com>
2022-06-22 21:49:37 -0600

    not found responsive styles

--- src/components/organisms/NotFound.tsx
+++ src/components/organisms/NotFound.tsx
@@ -1,13 +1,30 @@
 import { FC } from "react";
 import { css } from "@emotion/react";
 import Colors from "@app/styles/colors";
+import Screens from "@app/styles/breakpoints";
 import Image from "next/image";
 
 const NotFound: FC = () => {
   const containerStyles = css`
     cursor: default;
     display: flex;
-    gap: 1.5em;
+    align-items: center;
+    gap: 1.2em;
+    @media (min-width: ${Screens.sm}px) {
+      gap: 1.5em;
+    }
+  `;
+
+  const imageContainerStyles = css`
+    position: relative;
+    display: grid;
+    place-items: center;
+    width: 45px;
+    height: 45px;
+    @media (min-width: ${Screens.sm}px) {
+      width: 65px;
+      height: 65px;
+    }
   `;
 
   const textContainerStyles = css`
@@ -15,17 +32,26 @@ const NotFound: FC = () => {
     flex-direction: column;
     justify-content: center;
     gap: 0.4em;
-    max-width: 18.5em;
+    max-width: 14em;
+    @media (min-width: ${Screens.sm}px) {
+      max-width: 18.5em;
+    }
   `;
 
   const headlineStyles = css`
-    font-size: 1.5em;
+    font-size: 1.3em;
+    @media (min-width: ${Screens.sm}px) {
+      font-size: 1.5em;
+    }
     font-weight: 600;
     color: ${Colors.gray700};
   `;
 
   const descriptionStyles = css`
-    font-size: 1em;
+    font-size: 0.8em;
+    @media (min-width: ${Screens.sm}px) {
+      font-size: 1em;
+    }
     line-height: 1.5em;
     font-weight: 400;
     color: #b9b9b9;
@@ -33,12 +59,16 @@ const NotFound: FC = () => {
 
   return (
     <div css={containerStyles}>
-      <Image
-        src="/icons/questionMark.svg"
-        alt="not found"
-        width={70}
-        height={70}
-      />
+      <div css={imageContainerStyles}>
+        <Image
+          src="/icons/questionMark.svg"
+          alt="not found"
+          width="100%"
+          height="100%"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
       <div css={textContainerStyles}>
         <div css={headlineStyles}>No search results</div>
         <div css={descriptionStyles}>
